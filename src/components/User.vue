be425c2fb1c1f07851d5a1781d59d5c357935cdb
hanbi lee <46417675+leehanbi@users.noreply.github.com>
2019-03-10 16:10:57 +0900

    공부내용 커밋
    커밋에 빠진 파일들 다시 커밋.

---
+++ src/components/User.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="blue lighten-3 pa-3">
+    <h1>User 컴포넌트</h1>
+    <p>이름: {{name}}</p>
+    <p>성: {{familyName}}</p>
+      <v-btn color="info" @click="changeName()">이름 바꾸기</v-btn>
+    <hr>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <UserDetail :nameOfChild="name"></UserDetail>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <UserEdit></UserEdit>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue"
+import UserEdit from "./UserEdit.vue"
+
+//프롭스 : 부모 컴포넌트에서 자식 컴포넌트로 값을 전달하는 방법 
+// v-bind (:자식변수 = "어떤값을 줄 것인가(부모 컴포넌트) ")
+export default {
+  components: {
+    UserDetail,
+    UserEdit
+  },
+  data(){
+    return{
+      name:'마이클',
+      familyName: '숀'
+    }
+  },
+  methods: {
+    changeName (){
+      if(this.name == "hoza"){
+        this.name ="마이클"
+        this.familyName ="숀"
+      }else{
+        this.name = "hoza"
+        this.familyName ="Jack"
+      }
+    
+    }
+  }
+}
+</script>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="red lighten-3 pa-3">
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <p>{{nameOfChild}}</p>
+    <p>{{sayHellow}}</p>
+    <v-btn color="info" @click="switchName()">이름 바꾸기</v-btn>
+  </div>
+</template>
+
+
+<script>
+export default {
+  // 부모로부터 값을 받는 props
+  props:{
+    // 변수 : 변수타입 
+    // 기록을 해주는 이유는 데이터 타입을 알기 쉽게 위해서.
+    nameOfChild:{
+      type:String,
+      // 반드시 필요하다는 의미
+      required: true,
+      // 값을 전달하지 않을 경우 디폴트값 설정 가능 
+      default:'TEST'
+    }
+  },
+  computed:{
+    // 부모에게서 전달된 값을 this로 활용 가능.
+    sayHellow() {
+      return this.nameOfChild + '환영합니다.'
+    }
+  },
+  methods:{
+    switchName(){
+      this.nameOfChild = '제라드'
+    }
+  },
+  data(){
+    return{
+    }
+  }
+}
+</script>
